61fbc103f318932bbcda78108aaee96d9f667805
mac_a2141 <victor@piousbox.com>
2026-03-24 10:22:53 -0500

    a little cleanup

--- roles/ror/templates/config/initializers/action_mailer.rb
+++ roles/ror/templates/config/initializers/action_mailer.rb
@@ -1,18 +1,18 @@
 
-DEFAULT_RECIPIENT = 'poxlovi@gmail.com'
+DEFAULT_RECIPIENT = '$recipient'
 
 Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching       = false
   config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { :host => 'smt.creekenterprise.com' }
+  config.action_mailer.default_url_options   = { :host => '$host' }
   config.action_mailer.smtp_settings = {
     :port                 => 587,
     :authentication       => :login,
     :enable_starttls_auto => true,
     :address              => 'smtp.gmail.com',
-    :user_name            => 'no-reply@creekenterprise.com',
+    :user_name            => '$username',
     :password             => 'hm',
   }
 
