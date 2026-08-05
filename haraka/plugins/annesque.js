2e0f5a1733da6388097faeb54ecf7b35d0c0d883
mac_a2141 <victor@piousbox.com>
2026-08-05 13:46:58 -0500

    haraka bugfix

--- haraka/plugins/annesque.js
+++ haraka/plugins/annesque.js
@@ -1,19 +1,13 @@
-// annesque
-
-// documentation via: haraka -c /Users/piousbox/projects/ruby/annesque_email/haraka -h plugins/annesque
-
-// Put your plugin code here
-// type: `haraka -h Plugins` for documentation on how to create a plugin
 
 const BOUNCE = 'bounce'
 const SIMPLE_API_TOKEN = process.env.SIMPLE_API_TOKEN
 const ANNESQUE_ORIGIN  = process.env.ANNESQUE_ORIGIN
 
 exports.hook_mail = async function (next, connection, params) {
-  // this.loginfo('+++ params', params[0])
+  this.loginfo('+++ params', params[0])
 
   const sender_email = `${params[0].user}@${params[0].host}`.replace(/\+.*@/, "@").toLowerCase()
-  // this.loginfo('+++ sender_email: ', sender_email)
+  this.loginfo('+++ sender_email: ', sender_email)
 
   try {
     let response = await fetch(`${ANNESQUE_ORIGIN}/wco/api/leads/by-email/${sender_email}?secret=${SIMPLE_API_TOKEN}`)
@@ -21,13 +15,14 @@ exports.hook_mail = async function (next, connection, params) {
       return next()
     }
     response = await response.json()
+    this.loginfo('+++ lead from webui: ', response)
 
     const tags_slugs = response.tags.map(t => t.slug)
     if (tags_slugs.indexOf(BOUNCE) !== -1) {
       return next(DENY, "Denied due to policy.");
     }
 
-    const leadset_tags_slugs = response.tags.map(t => t.slug)
+    const leadset_tags_slugs = response.leadset.tags.map(t => t.slug)
     if (leadset_tags_slugs.indexOf(BOUNCE) !== -1) {
       return next(DENY, "Denied due to policy.");
     }
@@ -37,5 +32,5 @@ exports.hook_mail = async function (next, connection, params) {
     next()
   }
 
-  next();
+  next()
 };
